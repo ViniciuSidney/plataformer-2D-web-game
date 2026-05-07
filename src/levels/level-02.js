6fd3ce49c76d26756eec78ccf5663f7a5f8fbbf9
Vinícius Sidney <viniciusidney0@gmail.com>
2026-05-06 22:52:12 -0300

    feat: ajustar plataformas e adicionar coletáveis no level 2

--- src/levels/level-02.js
+++ src/levels/level-02.js
@@ -5,6 +5,7 @@ import {
    hole,
    groundWithHoles,
    hazard,
+   collectible,
 } from '../utils/levelBuilder.js';
 
 import { GAME_CONFIG } from '../config/gameConfig.js';
@@ -24,18 +25,19 @@ export const level02 = {
          hole(36, 8),
       ]),
 
-      platform(19, 29, 2, 1),
-
-      platform(12, 28, 2, 1),
       platform(13, 26, 1, 0.5),
       platform(8, 24, 3, 0.5),
-      platform(2, 24, 2, 0.5),
+      platform(2, 25, 2, 0.5),
       platform(0, 22, 1, 0.5),
+      platform(1, 23.5, 1, 0.5),
       platform(4, 20, 1, 0.5),
       platform(8, 18, 1, 0.5),
       platform(13, 17, 1, 0.5),
-
       platform(14, 16, 2, 12),
+
+      platform(11, 28, 3, 1),
+
+      platform(18.5, 29, 3, 1),
 
       platform(29, 29, 2, 1),
 
@@ -43,8 +45,15 @@ export const level02 = {
    ],
 
    hazards: [
-      //hazard(12, 27, 2, 1), 
+      //hazard(12, 27, 2, 1),
       //hazard(30, 27, 2, 1)
+   ],
+
+   collectibles: [
+      collectible(12, 27),
+      collectible(19.5, 28),
+      collectible(29.5, 28),
+      collectible(39.5, 28),
    ],
 
    goal: goal(45, 26, 1, 2),
